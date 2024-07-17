7077d2e127b823f65b30d3be0d1145d95fe3d186
SPC <SPC@21-LAB402>
2024-07-17 19:41:26 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b;
+       
+
+        Console.WriteLine("Nhap he so a: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Nhap he so b: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Nghiem cua PT la: " + GPTB1(a, b));
+
+    }
+
+    private static string GPTB1(double a, double b)
+    {
+        
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("PT vo so nghiem");
+            }
+            else
+            {
+                Console.WriteLine("PT vo nghiem");
+            }
+
+        }
+        else
+        {
+            double x = -b / a;
+            Console.WriteLine("Nghiem cua PT la: {0}", x);
+        }
+
+            return  " ";
+    }
+}
